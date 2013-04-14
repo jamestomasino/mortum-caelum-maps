c682c35d85c2efbf6f0a97b38c2e880ffd036b09
James Tomasino <james@tomasino.org>
2013-04-14 15:20:25 -0400

    adding all new files

---
+++ sjs/labeldata.js
@@ -0,0 +1,199 @@
+var countries = [];
+
+countries.push ( { 'name': 'Abawok', 'lat': -49.0955, 'lon': -24.0820 } );
+countries.push ( { 'name': 'Afona', 'lat': -38.62, 'lon': 142.21 } );
+countries.push ( { 'name': 'Anello', 'lat': 61.4808, 'lon': -42.8906 } );
+countries.push ( { 'name': 'Argo', 'lat': -32.0267, 'lon': 79.2773 } );
+countries.push ( { 'name': 'At', 'lat': 74.0437, 'lon': -161.8945 } );
+countries.push ( { 'name': 'Chimuna', 'lat': -20.1385, 'lon': -110.7422 } );
+countries.push ( { 'name': 'Cíthara', 'lat': 63.7436, 'lon': -89.4727 } );
+countries.push ( { 'name': 'Coilla', 'lat': 9.7957, 'lon': 88.4180 } );
+countries.push ( { 'name': 'Didumos', 'lat': 59.9731, 'lon': 81.5625 } );
+countries.push ( { 'name': 'Erebren', 'lat': 67.8424, 'lon': 118.1250 } );
+countries.push ( { 'name': 'Escala', 'lat': -10.5742, 'lon': -48.8672 } );
+countries.push ( { 'name': 'Flira', 'lat': 72.2355, 'lon': 160.9277 } );
+countries.push ( { 'name': 'Hare', 'lat': -17.3926, 'lon': 115.4883 } );
+countries.push ( { 'name': 'Karhuja', 'lat': 80.9423, 'lon': -32.3438 } );
+countries.push ( { 'name': 'Kerdo Republic', 'lat': 1.7425, 'lon': 158.8184 } );
+countries.push ( { 'name': 'Köttur', 'lat': 77.7862, 'lon': 58.2715 } );
+countries.push ( { 'name': 'Krios', 'lat': 61.2280, 'lon': 153.8086 } );
+countries.push ( { 'name': 'Leona', 'lat': 50.2332, 'lon': 23.2031 } );
+countries.push ( { 'name': 'Olora', 'lat': 80.0124, 'lon': -130.6055 } );
+countries.push ( { 'name': 'Phalaina', 'lat': -40.5806, 'lon': 163.5645 } );
+countries.push ( { 'name': 'Pabilsag', 'lat': -61.6064, 'lon': -141.2402 } );
+countries.push ( { 'name': 'Qartágo', 'lat': -63.7047, 'lon': -58.4473 } );
+countries.push ( { 'name': 'Salgar', 'lat': 15.8768, 'lon': 114.6973 } );
+countries.push ( { 'name': 'Vierra', 'lat': -19.8907, 'lon': -11.2500 } );
+countries.push ( { 'name': 'Zinkadin', 'lat': 80.0429, 'lon': 133.5938 } );
+
+var cities = [];
+
+// Abawok
+cities.push ( { 'name': 'Abowk', 'lat': -38.4106, 'lon': -27.2681 } )
+cities.push ( { 'name': 'Brusoot', 'lat': -58.4880, 'lon': -24.3237 } )
+cities.push ( { 'name': 'Kepeh', 'lat': -62.5680, 'lon': -5.0757 } )
+cities.push ( { 'name': 'Nahrina', 'lat': -58.4650, 'lon': -5.6909 } )
+cities.push ( { 'name': 'Tinh', 'lat': -44.3710, 'lon': -10.6787 } )
+
+// Afona
+cities.push ( { 'name': 'Penebyr', 'lat': -76.4758, 'lon': 169.0356 } );
+cities.push ( { 'name': 'Pentorlennydd', 'lat': -64.2255, 'lon': 140.4272 } );
+cities.push ( { 'name': 'Bachynys', 'lat': -46.6344, 'lon': 124.3652 } );
+cities.push ( { 'name': 'Iomys', 'lat': -36.1024, 'lon': 151.0840 } );
+cities.push ( { 'name': 'Ashydd', 'lat': -19.2282, 'lon': 155.6982 } );
+cities.push ( { 'name': 'Badaar-Torlan', 'lat': -17.0148, 'lon': 143.9209 } );
+cities.push ( { 'name': 'Cymeroma', 'align':'top', 'lat': -7.1445, 'lon': 142.6465 } );
+cities.push ( { 'name': 'Caethadra', 'align':'top', 'lat': -1.2743, 'lon': 136.5381 } );
+cities.push ( { 'name': 'Baeryn', 'align':'top', 'lat': -17.1828, 'lon': 151.0400 } );
+cities.push ( { 'name': 'Cymerbyr', 'lat': -7.7110, 'lon': 142.0313 } );
+cities.push ( { 'name': 'Torynys', 'align':'top', 'lat': -12.2971, 'lon': 129.1113 } );
+
+// Anello
+cities.push ( { 'name': 'Perlarotto', 'lat': 65.3851, 'lon': -26.0376 } )
+cities.push ( { 'name': 'Alpeggia', 'lat': 71.2019, 'lon': -34.2993 } );
+
+// Argo
+cities.push ( { 'name': 'Lightpool', 'lat': -36.9850, 'lon': 72.5757 } );
+cities.push ( { 'name': 'Brace', 'lat': -1.2743, 'lon': 76.3550 } );
+cities.push ( { 'name': 'Strathowe Word', 'lat': -34.4160, 'lon': 102.2827 } );
+cities.push ( { 'name': 'Pikeden', 'lat': -44.8091, 'lon': 88.0444 } );
+cities.push ( { 'name': 'Tyro', 'lat': -52.9883, 'lon': 101.4917 } );
+
+// At
+cities.push ( { 'name': 'Saq', 'lat': 74.9594, 'lon': -138.8672 } );
+cities.push ( { 'name': 'Serij', 'lat': 66.3728, 'lon': -142.7344 } );
+cities.push ( { 'name': 'Sur', 'lat': 69.5345, 'lon': -173.8477 } );
+cities.push ( { 'name': 'Menajun', 'lat': 53.2258, 'lon': -126.0352 } );
+cities.push ( { 'name': 'Elmashia', 'lat': 45.8288, 'lon': -101.9531 } );
+cities.push ( { 'name': 'Silsile', 'lat': 76.9206, 'lon': -171.0352 } );
+
+// Chimuna
+cities.push ( { 'name': 'Ealadubh', 'lat': -12.5975, 'lon': -132.4072 } )
+cities.push ( { 'name': 'Fabhacun', 'lat': -17.6859, 'lon': -129.5947 } )
+cities.push ( { 'name': 'Bhioma Scala', 'align':'top', 'lat': -6.7955, 'lon': -134.7363 } );
+
+// Cithara
+cities.push ( { 'name': 'Vultura', 'lat': 69.3803, 'lon': -76.6406 } );
+cities.push ( { 'name': 'Chelys', 'align':'top', 'lat': 55.6776, 'lon': -85.4297 } );
+cities.push ( { 'name': 'Testuda', 'lat': 53.2784, 'lon': -95.2734 } );
+cities.push ( { 'name': 'Ungula', 'lat': 68.8159, 'lon': -104.0625 } );
+
+// Coilla
+cities.push ( { 'name': 'Jiarga', 'lat': -1.5818, 'lon': 102.6563 } );
+cities.push ( { 'name': 'Çhaghter', 'lat': -7.3625, 'lon': 107.0508 } );
+cities.push ( { 'name': 'Trimmid', 'align':'top', 'lat': -2.7236, 'lon': 88.8574 } );
+cities.push ( { 'name': 'Keylley', 'lat': -3.6011, 'lon': 83.9355 } );
+cities.push ( { 'name': 'Roish yn Moddey', 'lat': 20.7150, 'lon': 84.8145 } );
+cities.push ( { 'name': 'Ben Keayney', 'lat': 24.4471, 'lon': 91.0547 } );
+cities.push ( { 'name': 'Moidynol', 'lat': -9.5357, 'lon': 90.7910 } );
+
+// Didumos
+cities.push ( { 'name': 'Tzafon Nahar', 'align':'top', 'lat': 69.7067, 'lon': 82.8589 } );
+cities.push ( { 'name': 'Astart', 'lat': 68.7841, 'lon': 78.6621 } );
+cities.push ( { 'name': 'Kabkeb', 'lat': 59.6844, 'lon': 100.3711 } );
+cities.push ( { 'name': 'Har Anbin', 'lat': 63.4652, 'lon': 81.7603 } );
+
+// Erebren
+cities.push ( { 'name': 'Bezan', 'lat': 71.7499, 'lon': 120.1904 } );
+cities.push ( { 'name': 'Dahanehu', 'lat': 70.2483, 'lon': 113.0933 } );
+cities.push ( { 'name': 'Boz', 'lat': 71.1345, 'lon': 124.6948 } );
+cities.push ( { 'name': 'Pai', 'lat': 58.3441, 'lon': 115.7080 } );
+cities.push ( { 'name': 'Mochedaz', 'lat': 64.7694, 'lon': 106.9189 } );
+cities.push ( { 'name': 'Darhen', 'lat': 61.6742, 'lon': 130.1880 } );
+
+// Escala
+cities.push ( { 'name': 'Tijeraña', 'lat': -9.9147, 'lon': -66.0278 } );
+cities.push ( { 'name': 'Suruña', 'lat': -24.4772, 'lon': -29.9268 } );
+cities.push ( { 'name': 'Nortescala', 'lat': 2.2736, 'lon': -53.9868 } );
+cities.push ( { 'name': 'Surartén', 'lat': -9.0262, 'lon': -33.9258 } );
+
+// Flira
+cities.push ( { 'name': 'Angonas', 'lat': 75.0220, 'lon': 157.9395 } );
+cities.push ( { 'name': 'Apodixiosteri', 'lat': 73.4847, 'lon': 161.0596 } );
+cities.push ( { 'name': 'Dexieri', 'lat': 75.8934, 'lon': 164.5313 } );
+cities.push ( { 'name': 'Podthi', 'align':'top', 'lat': 71.7808, 'lon': 152.2266 } );
+cities.push ( { 'name': 'Phterna', 'lat': 71.0812, 'lon': 150.8203 } );
+
+// Hare
+cities.push ( { 'name': 'Capon', 'lat': -8.2550, 'lon': 115.6201 } );
+cities.push ( { 'name': 'Wellspring', 'lat': -12.5331, 'lon': 120.1904 } );
+
+// Karhuja
+cities.push ( { 'name': 'Mustakar', 'lat': 77.6459, 'lon': -3.3398 } );
+cities.push ( { 'name': 'Lanne', 'lat': 75.3312, 'lon': -4.6582 } );
+cities.push ( { 'name': 'Pyrsta', 'lat': 77.3222, 'lon': -23.8184 } );
+cities.push ( { 'name': 'Lampatanta', 'align':'top', 'lat': 77.5326, 'lon': -32.7832 } );
+cities.push ( { 'name': 'Vyö', 'lat': 78.8955, 'lon': -40.6055 } );
+cities.push ( { 'name': 'Arkuneta', 'lat': 78.3405, 'lon': -57.7441 } );
+cities.push ( { 'name': 'Kuono', 'lat': 77.2254, 'lon': 27.5977 } );
+
+// Kerdo Republic
+cities.push ( { 'name': 'Opados', 'lat': 4.6750, 'lon': 148.9526 } );
+cities.push ( { 'name': 'Keirato', 'align':'top', 'lat': 9.7307, 'lon': 144.2065 } );
+cities.push ( { 'name': 'Korre', 'align':'top', 'lat': 4.2369, 'lon': 153.6108 } );
+cities.push ( { 'name': 'Mati', 'align':'top', 'lat': 8.2550, 'lon': 150.0952 } );
+cities.push ( { 'name': 'Pelitheo', 'lat': 8.4724, 'lon': 141.2183 } );
+
+// Kottur
+cities.push ( { 'name': 'Villtkött', 'lat': 74.4670, 'lon': 54.3384 } );
+
+// Krios
+cities.push ( { 'name': 'Kriokephale', 'lat': 61.6795, 'lon': 163.9819 } );
+cities.push ( { 'name': 'Thuosemeia', 'lat': 57.9615, 'lon': 172.5732 } );
+cities.push ( { 'name': 'Yepouro', 'lat': 54.8387, 'lon': 174.6606 } );
+cities.push ( { 'name': 'Aratro', 'lat': 67.1955, 'lon': 170.1343 } );
+
+// Leona
+cities.push ( { 'name': 'Piccolore', 'lat': 39.9771, 'lon': 45.1318 } );
+cities.push ( { 'name': 'Codaleoni', 'lat': 42.0330, 'lon': 13.8428 } );
+cities.push ( { 'name': 'Forza', 'lat': 47.1000, 'lon': 40.9131 } );
+cities.push ( { 'name': 'Treccia', 'align':'top', 'lat': 48.5748, 'lon': 44.2529 } );
+cities.push ( { 'name': 'Eunuco', 'lat': 66.6878, 'lon': 38.8037 } );
+
+// Olora
+cities.push ( { 'name': 'Caudagallina', 'lat': 81.3811, 'lon': -101.2061 } );
+cities.push ( { 'name': 'Rostrum', 'lat': 76.5066, 'lon': -86.5283 } );
+cities.push ( { 'name': 'Cistam', 'lat': 80.1712, 'lon': -97.1631 } );
+cities.push ( { 'name': 'Alam', 'lat': 79.0468, 'lon': -105.4248 } );
+cities.push ( { 'name': 'Equitum', 'lat': 80.9354, 'lon': -87.0557 } );
+
+// Phalaina
+cities.push ( { 'name': 'Ruthorni', 'lat': -22.7154, 'lon': 158.3350 } );
+cities.push ( { 'name': 'Terasthalas', 'lat': -31.1094, 'lon': 166.6626 } );
+cities.push ( { 'name': 'Batrahos', 'lat': -37.3876, 'lon': 169.2993 } );
+cities.push ( { 'name': 'Eperoha', 'lat': -27.9556, 'lon': 158.9502 } );
+
+// Pabilsag
+cities.push ( { 'name': 'Istanu', 'lat': -53.1468, 'lon': -123.4424 } );
+cities.push ( { 'name': 'Pusqu', 'lat': -58.0081, 'lon': -132.2314 } );
+cities.push ( { 'name': 'Erre', 'lat': -58.9273, 'lon': -115.8838 } );
+cities.push ( { 'name': 'Qaburu', 'lat': -62.0009, 'lon': -121.1572 } );
+
+// Qartágo
+cities.push ( { 'name': 'Nahal Nasar', 'lat': -60.3596, 'lon': -43.0005 } );
+cities.push ( { 'name': 'Nijic', 'lat': -54.4828, 'lon': -29.3774 } );
+cities.push ( { 'name': 'Mijleb', 'lat': -68.5684, 'lon': -88.3960 } );
+cities.push ( { 'name': 'Akal', 'lat': -66.1339, 'lon': -85.3198 } );
+
+// Salgar
+cities.push ( { 'name': 'Fin', 'lat': 28.0138, 'lon': 108.2153 } );
+cities.push ( { 'name': 'Vale', 'lat': -1.4391, 'lon': 133.7585 } );
+cities.push ( { 'name': 'Drusilla', 'lat': 24.1868, 'lon': 128.3862 } );
+cities.push ( { 'name': 'Groves', 'align':'top', 'lat': 5.6816, 'lon': 123.4204 } );
+cities.push ( { 'name': 'Posthlewaite', 'lat': 3.9739, 'lon': 120.6079 } );
+cities.push ( { 'name': 'Spathe', 'lat': 2.9760, 'lon': 119.0369 } );
+cities.push ( { 'name': 'Anliess', 'lat': -5.0800, 'lon': 117.2571 } );
+cities.push ( { 'name': 'Trimbley', 'lat': 34.6874, 'lon': 119.8169 } );
+cities.push ( { 'name': 'Cast', 'lat': -0.5383, 'lon': 121.1243 } );
+
+// Vierra
+cities.push ( { 'name': 'Épi', 'lat': -43.9295, 'lon': 11.6455 } );
+cities.push ( { 'name': 'Coin de Chien', 'lat': 5.5722, 'lon': 39.5947 } );
+cities.push ( { 'name': 'Aboyeur', 'lat': -20.0972, 'lon': 18.1494 } );
+cities.push ( { 'name': 'Vendangeur', 'lat': 2.4163, 'lon': -1.8018 } );
+
+// Zinkadin
+cities.push ( { 'name': 'Kusak', 'lat': 78.5168, 'lon': 148.0518 } );
+cities.push ( { 'name': 'Caracal', 'lat': 80.7111, 'lon': 118.5205 } );
+cities.push ( { 'name': 'Kuyruk', 'lat': 77.0098, 'lon': 162.3779 } );
+
---
+++ sjs/map.js
@@ -0,0 +1,240 @@
+var map = null;
+var mgr = null;
+var mapTypes = {};
+var mapTypeIds = [];
+var labels_countries = [];
+var labels_cities = [];
+
+mapTypes['mortumcaelum'] = {
+	getTileUrl: function(coord, zoom) {
+		return getHorizontallyRepeatingTileUrl(coord, zoom, function(coord, zoom) {
+			var bound = Math.pow(2, zoom);
+			var url = 'physical/' + zoom + "/" + coord.x + "/" + (bound - coord.y - 1) + '.png';
+			url = 'http://tomasino.org/maps/mortumcaelum/' + url;
+			return url;
+		});
+	},
+	tileSize: new google.maps.Size(256, 256),
+	isPng: true,
+	maxZoom: 6,
+	minZoom: 0,
+	radius: 6378000,
+	name: 'Mortum Caelum',
+	credit: 'Image Credit: James Tomasino'
+};
+
+function getHorizontallyRepeatingTileUrl(coord, zoom, urlfunc) {
+	var y = coord.y;
+	var x = coord.x;
+	var tileRange = 1 << zoom;
+	if (y < 0 || y >= tileRange) {
+		return null;
+	}
+	if (x < 0 || x >= tileRange) {
+		x = (x % tileRange + tileRange) % tileRange;
+	}
+	return urlfunc({x:x,y:y}, zoom)
+}
+
+function initialize() {
+	for (var key in mapTypes) {
+		mapTypeIds.push(key);
+	}
+	var mapOptions = {
+		zoom: 3,
+		panControl: false,
+		mapTypeControl: false,
+		streetViewControl: false,
+		overviewMapControl: false,
+		zoomControl: true,
+		scaleControl: true,
+		scaleControlOptions: {
+			position: google.maps.ControlPosition.BOTTOM_CENTER
+		},
+
+		center: new google.maps.LatLng(0, 0),
+		mapTypeControlOptions: {
+			mapTypeIds: mapTypeIds,
+		}
+	};
+	map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+	for (key in mapTypes) {
+		map.mapTypes.set(key, new google.maps.ImageMapType(mapTypes[key]));
+	}
+	map.setMapTypeId('mortumcaelum');
+
+
+	mgr = new MarkerManager(map);
+
+	google.maps.event.addListener(mgr, 'loaded', function(){
+		addMarkers();
+		mgr.refresh();
+		onZoomChange();
+	});
+
+//	google.maps.event.addListener(map, 'click', onClick );
+	google.maps.event.addListener(map, 'zoom_changed', onZoomChange );
+}
+
+function addMarkers () {
+	for ( var i = 0; i < countries.length; ++i ) {
+		var options = {
+			position: new google.maps.LatLng ( countries[i].lat, countries[i].lon ),
+			draggable: false,
+			labelContent: countries[i].name,
+			labelAnchor: new google.maps.Point(22,0),
+			labelClass: "label_country",
+			labelStyle: { opacity: 0.75 }
+		};
+		var marker = new MarkerWithLabel ( options );
+		marker.setIcon ( 'data:image/gif;base64,R0lGODlhAQABAIAAAAUEBAAAACwAAAAAAQABAAACAkQBADs=' ); // Blank gif base64
+		labels_countries.push (marker);
+		mgr.addMarker(marker, 0);
+	}
+
+	for ( var i = 0; i < cities.length; ++i ) {
+		var align = cities[i].align || 'bottom';
+				var options = {
+			position: new google.maps.LatLng ( cities[i].lat, cities[i].lon ),
+			draggable: false,
+			labelContent: cities[i].name,
+			labelClass: "label_city",
+			labelStyle: { opacity: 0.75 }
+		};
+		var marker = new MarkerWithLabel ( options );
+		marker.setIcon ( 'images/black-dot.png' ); // Blank gif base64
+		labels_cities.push (marker);
+		mgr.addMarker(marker, 3);
+	}
+}
+
+function onZoomChange (){
+	var zoom = map.getZoom();
+	var countrySize = 0;
+	var citySize = 0;
+	switch (zoom) {
+		case 0:
+			countrySize = 8;
+			citySize = 0;
+			break;
+		case 1:
+			countrySize = 12;
+			citySize = 0;
+			break;
+		case 2:
+			countrySize = 18;
+			citySize = 8;
+			break;
+		case 3:
+			countrySize = 24;
+			citySize = 12;
+			break;
+		case 4:
+			countrySize = 36;
+			citySize = 18;
+			break;
+		case 5:
+			countrySize = 48;
+			citySize = 24;
+			break;
+		case 6:
+			countrySize = 54;
+			citySize = 32;
+			break;
+	}
+
+	for (var i = 0; i < labels_countries.length; ++i ){
+		var marker = labels_countries[i];
+		marker.labelStyle = { 'opacity': 0.75, 'font-size': countrySize + 'px', 'height': (countrySize + 10) + 'px' };
+	}
+	for (var i = 0; i < labels_cities.length; ++i ){
+		var align = cities[i].align || 'bottom';
+		var marker = labels_cities[i];
+		var point;
+		switch ( align ) {
+			case 'top':
+				point = new google.maps.Point(22, (citySize + 8) * 1.25 );
+				break;
+			case 'left':
+				point = new google.maps.Point(22,0);
+				break;
+			case 'right':
+				point = new google.maps.Point(22,0);
+				break;
+			case 'bottom':
+			default:
+				point = new google.maps.Point(22,0);
+				break;
+		}
+
+		marker.labelStyle = { 'opacity': 0.75, 'font-size': citySize + 'px' };
+		marker.labelAnchor = point;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function onClick (event) {
+	placeMarker(event.latLng);
+}
+
+function onMarkerClick (event) {
+	this['infoWindow'].open(map, this);
+}
+
+function placeMarker(location) {
+	var title = "'lat': " + location.$a.toFixed(4) + ", 'lon': " + location.ab.toFixed(4);
+	var markerOptions = {};
+	markerOptions.title = title;
+	markerOptions.map = map;
+	markerOptions.position = location;
+
+	var marker = new google.maps.Marker(markerOptions);
+	google.maps.event.addListener(marker, 'click', onMarkerClick);
+	var infoWindowOptions = {};
+	infoWindowOptions.content = title;
+	marker['infoWindow'] = new google.maps.InfoWindow ( infoWindowOptions );
+	marker['infoWindow'].open(map, marker);
+
+	mgr.addMarker(marker, 4);
+	mgr.refresh();
+}
+
+function setupWeatherMarkers() {
+
+	}
+
